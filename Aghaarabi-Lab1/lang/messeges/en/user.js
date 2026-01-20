f445e7f2c59093a5ae7546027f0b145d79d5828a
faghaarabi <faghaarabi@my.bcit.ca>
2026-01-19 22:39:40 -0800

    Implement writer storage

---
+++ Aghaarabi-Lab1/lang/messeges/en/user.js
@@ -0,0 +1,16 @@
+
+
+const USER_MSG = Object.freeze({
+  APP_TITLE: "Lab 1: JSON, Object Constructor, localStorage",
+  STUDENT_NAME: "Student name : Fereshteh Aghaarabi", // <-- change this
+
+  BTN_WRITER: "Writer",
+  BTN_READER: "Reader",
+  BTN_BACK: "Back",
+  BTN_ADD: "add",
+  BTN_REMOVE: "remove",
+
+  SAVED_AT: "stored at:",
+  UPDATED_AT: "updated at:",
+  NO_NOTES: "No notes yet."
+});
